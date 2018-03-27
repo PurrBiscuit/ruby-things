d9905266a502c141bdeb3d84dda55d0fc4a6fce5
Purrbiscuit <spurr@articulate.com>
2018-03-27 10:40:56 -0400

    update grandma.rb

--- exercises/grandma.rb
+++ exercises/grandma.rb
@@ -1,20 +1,23 @@
 puts "Well hello, how are you?"
-response = gets.chomp
 bye_count = 0
 
-while bye_count < 2
+while true
+  response = gets.chomp
+
+  if response == "BYE"
+    bye_count += 1
+  else
+    bye_count = 0
+  end
+
+  if bye_count > 2
+    break
+  end
+
   if response != response.upcase
     puts "HUH!? SPEAK UP, SONNY!"
-    response = gets.chomp
-    bye_count = 0
-  elsif response == 'BYE'
-      bye_count += 1
-      puts "WHAT!?"
-      response = gets.chomp
   else
     puts "NO, NOT SINCE " + (1930 + rand(21)).to_s + "!"
-    response = gets.chomp
-    bye_count = 0
   end
 end
 
